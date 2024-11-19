3e308d8b15f2a3634c7b606e79f7113cec81e1c4
FanK <1258846421@qq.com>
2024-11-19 10:48:50 +0800

    fix - 支付逻辑

---
+++ backend/src/main/java/cc/mrbird/febs/cos/dao/OrderDetailMapper.java
@@ -0,0 +1,13 @@
+package cc.mrbird.febs.cos.dao;
+
+import cc.mrbird.febs.cos.entity.OrderDetail;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 订单详情 mapper层
+ *
+ * @author FanK wechat Storm_Berserker email fan1ke2ke@gmail.com
+ */
+public interface OrderDetailMapper extends BaseMapper<OrderDetail> {
+
+}
---
+++ backend/src/main/java/cc/mrbird/febs/cos/service/IOrderDetailService.java
@@ -0,0 +1,13 @@
+package cc.mrbird.febs.cos.service;
+
+import cc.mrbird.febs.cos.entity.OrderDetail;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * 订单详情 service层
+ *
+ * @author FanK wechat Storm_Berserker email fan1ke2ke@gmail.com
+ */
+public interface IOrderDetailService extends IService<OrderDetail> {
+
+}
